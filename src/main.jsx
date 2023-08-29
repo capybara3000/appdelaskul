9987b79230a9326138c0b6f2395d714d9988f7e5
capybara3000 <michelle@elpatojismo.edu.gt>
2023-08-29 15:35:00 -0600

    actualizaciondelaapp

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home2 from "./pages/Home2";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <Home2></Home2>
+  </div>
+);
